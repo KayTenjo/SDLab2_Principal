35d8efa464e2cf3fd6b0f84d5767e901a5d0c6b8
Rodrigo Monsalve <kayj.tune@gmail.com>
2013-12-17 13:48:39 -0300

    Nuevos xml yey

--- build/generated-sources/ap-source-output/entity/Usuario_.java
+++ build/generated-sources/ap-source-output/entity/Usuario_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-12-16T22:14:35")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-12-17T13:40:13")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
